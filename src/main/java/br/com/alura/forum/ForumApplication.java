68bfde9bc83c5f1aef5d3cf7cf5827cdcb614dc4
Rafael Simionato Hayashi <rafael.hayashi@amedigital.com>
2020-12-11 15:41:55 -0300

    Atualização Annotation SpringFox

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class ForumApplication {
 
 	public static void main(String[] args) {
